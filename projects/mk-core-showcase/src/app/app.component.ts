71ab1059c58bd59b228664cd759b310d53b84887
Mike Oliveira <mikeoliveira@outlook.com>
2024-07-07 00:31:37 -0300

    feat(file-structure): testando estrutura de arquivo

--- projects/mk-core-showcase/src/app/app.component.ts
+++ projects/mk-core-showcase/src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MkCoreSampleService } from 'mkCore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private mkCoreSampleService: MkCoreSampleService) {}
+
+  ngOnInit() {
+    console.log(this.mkCoreSampleService.getBestFramework());
+  }
+}
